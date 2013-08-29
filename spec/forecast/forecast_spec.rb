d57371dee501296dff3b3e590b1c9be121defd74
Ryan Vergeront <ryanvergeront@gmail.com>
2013-08-28 21:06:28 -0500

    refactor forecast spec

--- spec/forecast/forecast_spec.rb
+++ spec/forecast/forecast_spec.rb
@@ -4,16 +4,23 @@ require_relative '../assets/weather_hash_helper'
 module Forecast
   describe Forecast do
 
+    describe "creating a Forecast" do
+      it "creates a Location if one doesn't exist" do
+        Location.should_receive(:new)
+        forecast = Forecast.new
+      end
+    end
+
     before do
       @location = double("Location")
       @forecast = Forecast.new(@location)
     end
 
-    it 'responds to .summary' do
-      @forecast.should respond_to(:summary)
-    end
+    subject { @forecast }
+    it { should respond_to(:summary) }
+    it { should respond_to(:location) }
 
-    describe '.get_forecast' do
+    describe "getting data from the Forecast Client" do
       it 'sends a :fetch message to a Client object' do
         client = double("Client").stub(:fetch) { WeatherHashHelper.example }
         client.should_receive(:fetch).with(@location)
