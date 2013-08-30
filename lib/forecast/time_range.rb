28e84b6338ad8820634a4ad7e2e4c90d69edf64f
Ryan Vergeront <ryanvergeront@gmail.com>
2013-08-30 01:24:28 -0500

    TimeRange can init with Numerics (convert to Time)

--- lib/forecast/time_range.rb
+++ lib/forecast/time_range.rb
@@ -3,12 +3,21 @@ module Forecast
     attr_reader :start_time, :end_time
 
     def initialize(start_time = Time.now, end_time)
-      @start_time = start_time
-      @end_time = end_time
+      @start_time = convert_numerics_to_time(start_time)
+      @end_time = convert_numerics_to_time(end_time)
     end
 
     def duration
       ((end_time.to_i - start_time.to_i) / 60.0).round
+    end
+
+    private
+
+    def convert_numerics_to_time(time)
+      if time.is_a?(Numeric)
+        time = Time.at(time)
+      end
+      time
     end
   end
 end
--- spec/forecast/time_range_spec.rb
+++ spec/forecast/time_range_spec.rb
@@ -2,9 +2,17 @@ require_relative '../../lib/forecast'
 
 module Forecast
   describe TimeRange do
-    describe ".duration" do
-      let(:thirty_mins_from_now) { Time.at (Time.now.to_i + 30 * 60) }
+    describe "initialization" do
+      it "converts numerics to Time" do
+        range =TimeRange.new(1377801960, 1377801961)
+        expect(range.start_time).to be_kind_of Time
+        expect(range.end_time).to be_kind_of Time
+      end
+    end
 
+    let(:thirty_mins_from_now) { Time.at(Time.now.to_i + 30 * 60) }
+
+    describe "#duration" do
       it 'returns an Integer representing the number of minutes in the range' do
         thirty_mins = TimeRange.new(Time.now, thirty_mins_from_now)
         thirty_mins.duration.should be_within(1).of(30)
